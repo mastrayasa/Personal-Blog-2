86d3fd153bca35ae3f2cc74ed9b363f28058c531
Mastrayasa <mastrayasa@gmail.com>
2020-02-26 10:40:06 +0800

    Updates

---
+++ precache-manifest.4b2d3936db3900dc40b6251b96bef2d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5b5f0c8ff853986c2f1048a5ea6085",
+    "url": "/Personal-Blog-2/index.html"
+  },
+  {
+    "revision": "8cc5db2237768fd472b5",
+    "url": "/Personal-Blog-2/static/css/main.d4053e71.chunk.css"
+  },
+  {
+    "revision": "789818675aba36624870",
+    "url": "/Personal-Blog-2/static/js/2.a4b7b0b5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Personal-Blog-2/static/js/2.a4b7b0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc5db2237768fd472b5",
+    "url": "/Personal-Blog-2/static/js/main.ab9970aa.chunk.js"
+  },
+  {
+    "revision": "bed01af6070ca8215b51",
+    "url": "/Personal-Blog-2/static/js/runtime-main.231e44d8.js"
+  },
+  {
+    "revision": "401b8046ebe78025e50360e941b71ee7",
+    "url": "/Personal-Blog-2/static/media/baner.401b8046.jpg"
+  },
+  {
+    "revision": "29d46f09e5515d2d6f13df1e9ad46e06",
+    "url": "/Personal-Blog-2/static/media/saya.29d46f09.jpg"
+  }
+]);
