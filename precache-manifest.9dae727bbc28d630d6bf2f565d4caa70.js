24b0bcf20ec5e3237dfe52e936cf98e7fe5cad8f
Mastrayasa <mastrayasa@gmail.com>
2020-02-26 10:35:01 +0800

    Updates

---
+++ precache-manifest.9dae727bbc28d630d6bf2f565d4caa70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3006e8e795db7f69cf509a8c25292f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f34b1a071558ceffcd74",
+    "url": "/static/css/main.d4053e71.chunk.css"
+  },
+  {
+    "revision": "789818675aba36624870",
+    "url": "/static/js/2.a4b7b0b5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a4b7b0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34b1a071558ceffcd74",
+    "url": "/static/js/main.cab44aaf.chunk.js"
+  },
+  {
+    "revision": "00ce8ae3c4525f9b5866",
+    "url": "/static/js/runtime-main.69045b3e.js"
+  },
+  {
+    "revision": "401b8046ebe78025e50360e941b71ee7",
+    "url": "/static/media/baner.401b8046.jpg"
+  },
+  {
+    "revision": "29d46f09e5515d2d6f13df1e9ad46e06",
+    "url": "/static/media/saya.29d46f09.jpg"
+  }
+]);
